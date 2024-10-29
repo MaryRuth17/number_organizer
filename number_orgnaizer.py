6973ccd58b8c89a7472a86425386bb70b2d6dda1
Mary Ruth <[maryruth.pesidas.relator@gmail.com]>
2024-10-29 14:38:14 +0800

    putting an array and range

--- number_orgnaizer.py
+++ number_orgnaizer.py
@@ -1,3 +1,24 @@
+#setting up an array with ranges
+num_range = {
+        "1-10" : 0,
+        "11-20" : 0,
+        "21-30" : 0,
+        "31-40" : 0,
+        "41-50" : 0
+    }
 
 # ask user to input numbers
-num = int(input("Enter a number between 1 and 50 only: "))
+num = int(input("Enter a number (1 and 50 only): "))
+if (num>=1) and (num<=50): #putting the numbers in range using if else
+    if (num>=1) and (num<=10):
+        num_range["1-10"] += 1
+    elif (num>=11) and (num<=20):
+        num_range["11-20"] += 1
+    elif (num>=21) and (num<=30):
+        num_range["21-30"] += 1
+    elif (num>=31) and (num<=40):
+        num_range["31-40"] += 1
+    elif (num>=41) and (num<=50):
+        num_range["41-50"] += 1
+else:
+    print("invalid")
